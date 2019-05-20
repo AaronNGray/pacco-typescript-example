da1be019148f8b84323cea78cc3c17a883ae1735
AaronNGray <aaronngray@gmail.com>
2019-05-20 23:04:03 +0100

    Initial commit

---
+++ src/test.ts
@@ -0,0 +1,11 @@
+module test {
+    export class Test {
+        constructor(options) {
+            console.log("Test.constructor()");
+
+        }
+        test() {
+            console.log("Test.test()");
+        }
+    };
+}
